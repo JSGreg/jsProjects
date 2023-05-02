a8324d27c3f1cdee8cd7ee1f1847dbd50f7617d4
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-05-02 20:03:34 +1000

    Colour flipper
    created two types of background colour flippers
    - Array
    - Random

---
+++ backgroundChanger/app.js
@@ -0,0 +1,23 @@
+// arry for holding basic colour types
+const colorsArr = ["orange", "blue", "rgba(13, 152, 30)", "#FFFFFF" ];
+
+// button gets the first thing with the btn attribute
+const btn = document.getElementById('btn');
+// returns first element of matching desendant node 
+const color =document.querySelector(".color");
+
+// listens for button clicks
+btn.addEventListener("click", function( ) {
+    // log
+    console.log(document.body);
+    // random num 0-3
+    const randomNumber = getRandomNum();
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colorsArr[randomNumber];
+});
+
+function getRandomNum(){
+    return Math.floor(Math.random()*colorsArr.length);
+}
